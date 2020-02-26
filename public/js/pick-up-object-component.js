ab782f0c024593510b316c770e286f638360e105
Yefang Kuang <kuan0008@algonquinlive.com>
2020-02-26 15:36:16 -0500

    Start Menu executes functions

--- public/js/pick-up-object-component.js
+++ public/js/pick-up-object-component.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent('pick-up-object', {
     schema : {
-        cursor: {type: 'selector', default: "#main-cursor"},
+        cursor: {type: 'selector', default: "#game-cursor"},
     },
 
     init : function() {
@@ -38,9 +38,7 @@ AFRAME.registerComponent('pick-up-object', {
 
     //This is how the object that is selected will be follow the mouse
     pickUpObject: function(){
-        console.log("pick up Object function");
-        var camera = document.querySelector("#primary-camera");
-        var scene = document.getElementById("scene");
+        var camera = document.querySelector("#game-camera");
         this.el.removeAttribute("dynamic-body");
         this.el.object3D.position.set(0,0,-2)
         this.el.object3D.rotation.x = 0;
@@ -50,13 +48,10 @@ AFRAME.registerComponent('pick-up-object', {
     },
 
     dropObject: function(){
-        console.log("drop object");
-        var camera = document.querySelector("#primary-camera");
-        var scene = document.getElementById("scene");
+        var scene = document.getElementById("ingame");
         this.el.object3D.getWorldPosition(this.el.object3D.position);
         this.el.object3D.getWorldQuaternion(this.el.object3D.rotation);
         this.el.setAttribute("dynamic-body", '');
         scene.object3D.attach(this.el.object3D);
-        
     }
 });
