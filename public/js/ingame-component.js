791beeaa7f0333d73d4c59d8ee81f5a83ff6b44e
Yefang Kuang <kuan0008@algonquinlive.com>
2020-03-06 12:10:09 -0500

    refined hud, added score functionality

--- public/js/ingame-component.js
+++ public/js/ingame-component.js
@@ -1,14 +1,23 @@
 AFRAME.registerComponent('ingame', {
     schema : {
         time : {type: 'int', default: 12000},
+        score : {type: 'int', default: 0},
     },
 
     init : function() {
-       
+        let timerEl = document.querySelector("#timer");
+        let scoreEl = document.querySelector("#score");
+
+        let hudX = (document.body.clientWidth / 2) / 960;
+        let hudY = ((document.body.clientHeight / 2) / 540) / 2;
+
+        timerEl.setAttribute("position", "-" + hudX + " " + hudY + " -1");
+        scoreEl.setAttribute("position", hudX + " " + hudY + " -1");
     },
 
     tick : function(){
         let timerEl = document.querySelector("#timer");
+        let scoreEl = document.querySelector("#score");
         
         if(this.data.time <= 0) {
             console.log("victory");
@@ -16,5 +25,7 @@ AFRAME.registerComponent('ingame', {
             this.data.time--;
             timerEl.setAttribute("value", Math.floor(this.data.time / 100));
         }
+
+        scoreEl.setAttribute("value", this.data.score + " PTS");
     }
 });
