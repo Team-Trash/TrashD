b9de5d8daaaa54bb042c92ae480ceb5e10354092
Hoang Nguyen <nguy0670@algonquinlive.com>
2020-03-11 15:15:26 -0400

    Remove unnecessary

--- public/js/generating-trash.js
+++ public/js/generating-trash.js
@@ -44,19 +44,6 @@ function gtFunction(){
                 break;
         }
         trashID += 1;
-
-        //var trash = document.createElement('a-entity');
-        //trash.setAttribute('class',trashes[0].classTrash);
-        //trash.setAttribute('geometry', 'primitive: box; depth: 1; height: 1; width: 1');
-        //trash.setAttribute('position', trashes[0].positionX , trashes[0].positionY, trashes[0].positionZ);
-        //trash.setAttribute('scale', '0.5 0.5 0.5');
-        //trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
-        //trash.setAttribute('pick-up-object', '');
-
-        //scene.append(trashes[0]);
-        //console.log(trash);
-        //REMOVE THIS SO IT CAN CONTINUELY ADDING NEW TRASH
-        //clearInterval(generatingTrash);
     }
 
     function deletingTrashFunction(){
