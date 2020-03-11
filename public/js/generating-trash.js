eea3cfc8f3d952010db4aecf9784a2f96d80b905
Hoang Nguyen <nguy0670@algonquinlive.com>
2020-03-11 15:08:08 -0400

    Deleteing trash object for js

--- public/js/generating-trash.js
+++ public/js/generating-trash.js
@@ -1,41 +1,68 @@
 function gtFunction(){
+    let trashID = 0;
+    let trashDeletingID = 0;
     var trashType = ["plastic", "metal", "compost", "paper", "trash", "glass"];
+    let trashes = [];
     var scene = document.getElementById('scene');
     generatingTrash = setInterval(generatingTrashFunction, 3000);
+    deletingTrash = setInterval(deletingTrashFunction, 10000);
 
 
     function generatingTrashFunction(){
         //Get the random number
-        var randomNum = Math.floor(Math.random() * 60);
-        if(0 < randomNum < 10){
-            console.log("1 to 10");
+        var randomNum = Math.floor(Math.random() * 5);
+        switch(randomNum) {
+            case 0:
+                plasticTrash = new Trash('plastic', "trash" + trashID);
+                trashes.push(plasticTrash.generatingAttribute());
+                scene.append(plasticTrash.generatingAttribute());
+                break;
+            case 1:
+                metalTrash = new Trash('metal', "trash" + trashID);
+                trashes.push(metalTrash.generatingAttribute());
+                scene.append(metalTrash.generatingAttribute());
+                break;
+            case 2:
+                compostTrash = new Trash('compost',"trash" + trashID);
+                trashes.push(compostTrash.generatingAttribute());
+                scene.append(compostTrash.generatingAttribute());
+                break;
+            case 3:
+                paperTrash = new Trash('paper', "trash" + trashID);
+                trashes.push(paperTrash.generatingAttribute());
+                scene.append(paperTrash.generatingAttribute());
+                break;
+            case 4:
+                trashTrash = new Trash('trash',"trash" + trashID);
+                trashes.push(trashTrash.generatingAttribute());
+                scene.append(trashTrash.generatingAttribute());
+                break;
+            case 5:
+                glassTrash = new Trash('glass',"trash" + trashID);
+                trashes.push(glassTrash.generatingAttribute());
+                scene.append(glassTrash.generatingAttribute());
+                break;
         }
-        else if(10 < randomNum < 20){
-            console.log("10 to 20");
-        }
-        else if(20 < randomNum < 30){
-            console.log("20 to 30");
-        }
-        else if(30 < randomNum < 40){
-            console.log("30 to 40");
-        }
-        else if(40 < randomNum < 50){
-            console.log("40 to 50");
-        }
-        else if(50 < randomNum < 60){
-            console.log("50 to 60");
-        }
-        var trash = document.createElement('a-entity');
-        trash.setAttribute('class', 'clickable');
-        trash.setAttribute('geometry', 'primitive: box; depth: 1; height: 1; width: 1');
-        trash.setAttribute('position', '-5 2 -3');
-        trash.setAttribute('scale', '0.5 0.5 0.5');
-        trash.setAttribute('rotation', '0 0 0');
-        trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
-        trash.setAttribute('pick-up-object', '');
+        trashID += 1;
 
-        scene.append(trash);
+        //var trash = document.createElement('a-entity');
+        //trash.setAttribute('class',trashes[0].classTrash);
+        //trash.setAttribute('geometry', 'primitive: box; depth: 1; height: 1; width: 1');
+        //trash.setAttribute('position', trashes[0].positionX , trashes[0].positionY, trashes[0].positionZ);
+        //trash.setAttribute('scale', '0.5 0.5 0.5');
+        //trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
+        //trash.setAttribute('pick-up-object', '');
+
+        //scene.append(trashes[0]);
+        //console.log(trash);
         //REMOVE THIS SO IT CAN CONTINUELY ADDING NEW TRASH
         //clearInterval(generatingTrash);
     }
+
+    function deletingTrashFunction(){
+        var trashDeleting = document.getElementById("trash" +trashDeletingID);
+        trashDeleting.parentNode.removeChild(trashDeleting);
+        trashDeletingID += 1;
+    }
+
 }
--- public/js/trash-class.js
+++ public/js/trash-class.js
@@ -1,5 +1,25 @@
 class Trash{
-    constructor(type){
+    classTrash = 'clickable';
+    positionX = '-5';
+    positionY = '2';
+    positionZ = '-3';
+    scaleX = 0.5;
+    scaleY = 0.5;
+    scaleZ = 0.5;
+    constructor(type, id){
         this.type = type;
+        this.id = id;
+    }
+    
+    generatingAttribute(){
+        var trash = document.createElement('a-entity');
+        trash.setAttribute('id', this.id);
+        trash.setAttribute('class', this.classTrash);
+        trash.setAttribute('geometry', 'primitive: box; depth: 1; height: 1; width: 1');
+        trash.setAttribute('position', this.positionX , this.positionY, this.positionZ);
+        trash.setAttribute('scale', '0.5 0.5 0.5');
+        trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
+        trash.setAttribute('pick-up-object', '');
+        return trash;
     }
 }
