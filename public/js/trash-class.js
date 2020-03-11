6c9a2c5909d03172606157e12ef37631f0145ddd
Hoang Nguyen <nguy0670@algonquinlive.com>
2020-03-11 15:46:33 -0400

    Fixing

--- public/js/trash-class.js
+++ public/js/trash-class.js
@@ -33,99 +33,39 @@ class Trash{
         return trash;
     }
     //GET THE PLASTIC MODELS
-    generatingAttribute(){
+    generatingAttribute2(){
         var trash = document.createElement('a-entity');
         //Set random number for texture and models.
         var randomNum = Math.floor(Math.random() * 5);
         switch(this.type){
             case 'plastic':
-                trash.setAttribute('obj-model', 'obj:');
-                trash.setAttribute('maetrial', 'src:');
+                //trash.setAttribute('obj-model', 'obj:');
+                //trash.setAttribute('maetrial', 'src:');
                 break;
             case 'metal':
-                trash.setAttribute('obj-model', 'obj:');
-                trash.setAttribute('maetrial', 'src:');
+                //trash.setAttribute('obj-model', 'obj:');
+                //trash.setAttribute('maetrial', 'src:');
                 break;
             case 'compost':
-                trash.setAttribute('obj-model', 'obj:');
-                trash.setAttribute('maetrial', 'src:');
+                //trash.setAttribute('obj-model', 'obj:');
+                //trash.setAttribute('maetrial', 'src:');
                 break;
             case 'paper':
-                trash.setAttribute('obj-model', 'obj:');
-                trash.setAttribute('maetrial', 'src:');
+                //trash.setAttribute('obj-model', 'obj:');
+                //trash.setAttribute('maetrial', 'src:');
                 break;
             case 'trash':
-                trash.setAttribute('obj-model', 'obj:');
-                trash.setAttribute('maetrial', 'src:');
+                //trash.setAttribute('obj-model', 'obj:');
+                //trash.setAttribute('maetrial', 'src:');
                 break;
             case 'glass':
-                trash.setAttribute('obj-model', 'obj:');
-                trash.setAttribute('maetrial', 'src:');
+                //trash.setAttribute('obj-model', 'obj:');
+                //trash.setAttribute('maetrial', 'src:');
                 break;
         }
 
         trash.setAttribute('id', this.id);
         trash.setAttribute('class', this.classTrash);
-        trash.setAttribute('position', this.positionX , this.positionY, this.positionZ);
-        trash.setAttribute('scale', '0.5 0.5 0.5');
-        trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
-        trash.setAttribute('pick-up-object', '');
-        return trash;
-    }
-    generatingMetal(){
-        var trash = document.createElement('a-entity');
-        trash.setAttribute('id', this.id);
-        trash.setAttribute('class', this.classTrash);
-        trash.setAttribute('obj-model', 'obj:');
-        trash.setAttribute('maetrial', 'src:');
-        trash.setAttribute('position', this.positionX , this.positionY, this.positionZ);
-        trash.setAttribute('scale', '0.5 0.5 0.5');
-        trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
-        trash.setAttribute('pick-up-object', '');
-        return trash;
-    }
-    generatingCompost(){
-        var trash = document.createElement('a-entity');
-        trash.setAttribute('id', this.id);
-        trash.setAttribute('class', this.classTrash);
-        trash.setAttribute('obj-model', 'obj:');
-        trash.setAttribute('maetrial', 'src:');
-        trash.setAttribute('position', this.positionX , this.positionY, this.positionZ);
-        trash.setAttribute('scale', '0.5 0.5 0.5');
-        trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
-        trash.setAttribute('pick-up-object', '');
-        return trash;
-    }
-    generatingPaper(){
-        var trash = document.createElement('a-entity');
-        trash.setAttribute('id', this.id);
-        trash.setAttribute('class', this.classTrash);
-        trash.setAttribute('obj-model', 'obj:');
-        trash.setAttribute('maetrial', 'src:');
-        trash.setAttribute('position', this.positionX , this.positionY, this.positionZ);
-        trash.setAttribute('scale', '0.5 0.5 0.5');
-        trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
-        trash.setAttribute('pick-up-object', '');
-        return trash;
-    }
-    generatingTrash(){
-        var trash = document.createElement('a-entity');
-        trash.setAttribute('id', this.id);
-        trash.setAttribute('class', this.classTrash);
-        trash.setAttribute('obj-model', 'obj:');
-        trash.setAttribute('maetrial', 'src:');
-        trash.setAttribute('position', this.positionX , this.positionY, this.positionZ);
-        trash.setAttribute('scale', '0.5 0.5 0.5');
-        trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
-        trash.setAttribute('pick-up-object', '');
-        return trash;
-    }
-    generatingGlass(){
-        var trash = document.createElement('a-entity');
-        trash.setAttribute('id', this.id);
-        trash.setAttribute('class', this.classTrash);
-        trash.setAttribute('obj-model', 'obj:');
-        trash.setAttribute('maetrial', 'src:');
         trash.setAttribute('position', this.positionX , this.positionY, this.positionZ);
         trash.setAttribute('scale', '0.5 0.5 0.5');
         trash.setAttribute('animation', 'property: position; from: -5.5 1 -3; to: 7 1 -3; dur: 7000; easing: linear');
